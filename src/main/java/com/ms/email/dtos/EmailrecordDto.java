48e2bbc6d8cd8641a7a1c9b43ec6feb00c213997
Fernanda Pestana <pestanafj@gmail.com>
2024-05-02 23:47:45 -0300

    Configurado o envio do email

--- src/main/java/com/ms/email/dtos/EmailrecordDto.java
+++ src/main/java/com/ms/email/dtos/EmailrecordDto.java
@@ -2,7 +2,7 @@ package com.ms.email.dtos;
 
 import java.util.UUID;
 
-public record EmailrecordDto(UUID userId,
+public record EmailRecordDto(UUID userId,
                              String emailTo,
                              String subject,
                              String text) {
